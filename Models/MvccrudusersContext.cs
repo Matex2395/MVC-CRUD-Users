17c04dbb02746db67720ca0b8cab44c9f14621c4
Mateo Rosero <mateorgafjo@gmail.com>
2024-10-02 12:46:40 -0500

    Instalación Paquetes & Conexión a Base de Datos Local

---
+++ Models/MvccrudusersContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_CRUD_Users.Models;
+
+public partial class MvccrudusersContext : DbContext
+{
+    public MvccrudusersContext()
+    {
+    }
+
+    public MvccrudusersContext(DbContextOptions<MvccrudusersContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Initial Catalog=MVCCRUDUSERS;User ID=MateoSA;Password=empanada123;Encrypt=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07577910A5");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D10534246021E3").IsUnique();
+
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(255);
+            entity.Property(e => e.Role)
+                .HasMaxLength(50)
+                .HasDefaultValue("User");
+            entity.Property(e => e.UpdatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MVC_CRUD_Users.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MvccrudusersContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CRUDConection")));
 
 var app = builder.Build();
 
