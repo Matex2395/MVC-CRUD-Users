a62da846c45559a9ab8c3b9a0a13fca506d875a9
Mateo Rosero <mateorgafjo@gmail.com>
2024-10-02 12:33:08 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_CRUD_Users.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
