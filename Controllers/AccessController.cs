e525edf2d5a6883b2e1f44dcf6652526a46dc307
Mateo Rosero <mateorgafjo@gmail.com>
2024-10-09 09:39:17 -0500

    Controlador de Acceso y Vista de Registro creados.

---
+++ Controllers/AccessController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_CRUD_Users.Models;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace MVC_CRUD_Users.Controllers
+{
+    private readonly MvccrudusersContext _db;
+    public class AccessController : Controller
+    {
+        public AccessController(MvccrudusersContext context)
+        {
+            _db = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
