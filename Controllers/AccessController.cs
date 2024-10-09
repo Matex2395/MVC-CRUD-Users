7d1d12800f2e2b5597fa81801dcf3cf25d92468e
Mateo Rosero <mateorgafjo@gmail.com>
2024-10-09 09:57:59 -0500

    Formulario Registro completo

--- Controllers/AccessController.cs
+++ Controllers/AccessController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 using MVC_CRUD_Users.Models;
-using Microsoft.EntityFrameworkCore.SqlServer;
+using MVC_CRUD_Users.ViewModels;
 
 namespace MVC_CRUD_Users.Controllers
 {
-    private readonly MvccrudusersContext _db;
     public class AccessController : Controller
     {
+        private readonly MvccrudusersContext _db;
+
         public AccessController(MvccrudusersContext context)
         {
             _db = context;
@@ -15,6 +23,34 @@ namespace MVC_CRUD_Users.Controllers
         [HttpGet]
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserVM model)
+        {
+            if(model.Password != model.ConfirmPassword)
+            {
+                ViewData["Message"]= "Las contraseñas no coinciden";
+                return View();
+            }
+
+            User user = new User
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Password = model.Password,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+                Role = "User"
+            };
+
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
+
+            if(user.Id != 0) return RedirectToAction("Login","Access");
+
+            ViewData["Message"] = "Ha ocurrido un error al registrar el usuario.";
             return View();
         }
     }
